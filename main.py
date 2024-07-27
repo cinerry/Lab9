0b0d9ab962c8c1ef31ec739a6901de7285d0db69
christopheranderson <christopher33154@gmail.com>
2024-07-27 17:07:19 -0400

    I added the menu to finish the code.

--- main.py
+++ main.py
@@ -12,3 +12,30 @@ def decode(password):
         changed_char = (int(char) - 3) % 10
         empty_string += str(changed_char)
     return empty_string
+
+def main ():
+    encode_password = ""
+    while True:
+        print("Menu")
+        print("-------------")
+        print("1. Encode")
+        print("3. Decode")
+        print("3. Quit")
+        option = input("Please enter an option: ")
+
+        if option == "1":
+            password = input("Please enter the password to encode: ")
+            encode_password = encode(password)
+            print("Your password has been encoded and stored!\n")
+
+        elif option == "2":
+            if encode_password:
+                decode_password = decode(encode_password)
+                print(f"The encoded password is {encode_password}, and the original password is {decode_password}.\n")
+            else:
+                print("Enter encoded password first.")
+
+        elif option == "3":
+            break
+if __name__ == "__main__":
+    main()
